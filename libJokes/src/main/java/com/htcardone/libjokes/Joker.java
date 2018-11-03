8db000165b552f14ee9122ed874bc0e7d1a385ef
Debjyoti Guha <debajyotiguha11@gmail.com>
2018-11-04 00:02:50 +0530

    Add files via upload

--- libJokes/src/main/java/com/htcardone/libjokes/Joker.java
+++ libJokes/src/main/java/com/htcardone/libjokes/Joker.java
@@ -1,7 +1,15 @@
 package com.htcardone.libjokes;
+import java.util.Random;
 
 public class Joker {
+    public static String[] jokes = {
+            "joke 1",
+            "joke 2",
+            "joke 3",
+            "joke 4",
+            "joke 5"};
+
     public static String getJoke() {
-        return "This is totally a funny joke from my lib";
+        return jokes[new Random().nextInt(jokes.length)];
     }
 }
